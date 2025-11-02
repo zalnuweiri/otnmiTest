0284474d5734dd594d09370939f8c90b4f3d7429
Zayd A <zy854413@dal.ca>
2025-11-02 16:09:00 -0500

    updated api.js with not hardcoded url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5050/api", // hardcoded temporarily
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5050/api",
 });
 
 export default api;
