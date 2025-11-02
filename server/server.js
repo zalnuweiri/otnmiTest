dda5745daa3b6303189cfe1455b6b54802bb0980
Zayd A <zy854413@dal.ca>
2025-11-02 15:18:26 -0500

    updated server.js

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(
         origin: [
             "http://localhost:5173",
             "http://localhost:5174",
-            "https://ottonomi.netlify.app",
+            "https://ottonomi.netlify.app"
         ],
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
